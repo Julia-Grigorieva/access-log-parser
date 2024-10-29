2a9767d739ad7b64d7c2ef52f207fcf1a355127c
iuliia_grigoreva <grigorievajulia11.89@yandex.ru>
2024-10-29 23:40:11 +0300

    added file path checks

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,24 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите первое число: ");
-        int firstNumber = new Scanner(System.in).nextInt();
-        System.out.println("Введите второе число: ");
-        int secondNumber = new Scanner(System.in).nextInt();
-        int sum = firstNumber+secondNumber;
-        int difference = firstNumber-secondNumber;
-        int multiplication = firstNumber*secondNumber;
-        double quotient = (double)firstNumber/secondNumber;
-        System.out.println("Сумма чисел: " + sum);
-        System.out.println("Разность чисел: " + difference);
-        System.out.println("Произведение чисел: " + multiplication);
-        System.out.println("Частное чисел: " + quotient);
+       int count = 0;
+       while(true){
+           System.out.println("Введите путь к файлу:");
+           String path = new Scanner(System.in).nextLine();
+           File file = new File(path);
+           boolean fileExists = file.exists();
+           boolean isDirectory = file.isDirectory();
+           if(!fileExists || isDirectory){
+               System.out.println("указанный файл не существует или указанный путь является путём к папке");
+               continue;
+           }
+           if (fileExists) {
+               System.out.println("Путь указан верно");
+               count ++;
+               System.out.println("Это файл номер " + count);
+           }
+       }
     }
 }
